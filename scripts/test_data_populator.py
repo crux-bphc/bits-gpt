2673510f6ad5851e45940aa74707321a1caf8341
OpexRah <dougisrave@gmail.com>
2024-01-26 12:49:34 +0530

    feat: Add test data and script
    the script is to make it easy to add new test data

---
+++ scripts/test_data_populator.py
@@ -0,0 +1,38 @@
+import json
+
+'''
+{
+  "simple-facts": [],
+  "complex-facts": [],
+  "advice": [],
+  "opinion": []
+}
+'''
+
+def addData(category, question, answer):
+    with open("./tests/tests.json", "r") as handle:
+        data = json.load(handle)
+
+    if category not in data:
+        print("Category invalid")
+        return
+    
+    data[category].append({"question":question, "expected_answer":answer})
+    with open("./tests/tests.json", "w") as output:
+        json.dump(data, output, indent=2)
+
+def main():
+    while True:
+        print("type 'exit' to quit\n\n")
+        category = input("Enter category: ")
+        question = input("Enter question: ")
+        answer = input("Enter answer: ")
+
+        if category == 'exit' or question == 'exit' or answer == 'exit':
+            break
+
+        addData(category, question, answer)
+
+
+if __name__ == "__main__":
+    main()
